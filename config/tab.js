e660c51b2ca83965b683e4343886cd29c9e2580c
笑笑 <819632763@qq.com>
2019-12-27 17:11:24 +0800

    主页

---
+++ config/tab.js
@@ -0,0 +1,32 @@
+var datas = [
+	{
+		title:'好人一生平安',
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTA0OTM2NTM5NQ==-signbfdf1f46f6dd45bbfc4ba25efe14d755?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+	},
+	{
+		title:'像鱼',
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTAzNjAyNjg0Mw==-sign0fc9d189d9a18aa29c95c62ea7f882f7?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+	},
+	{
+		title:'胡广生',
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTAzNjAxOTkwNQ==-sign465eb56081a31681c28c1c2336fa55a7?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+	},
+	{
+		title:'万爱千恩',
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTAwMDM2ODM0MA==-sign465c8309b52ff3820a93213ae17891a2?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+	},
+	{
+		title:'多年以后',
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataOTk3OTI1NDk2-sign643f30db5ffe19d58b03f1d1fc63a390?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+	},
+	{
+		title:'成都',
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataOTgwNzAyNTgy-signeae40a3c568d2a5ad5ecccff5e9e8aab?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+	},
+	{
+		title:'西海情歌',
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataOTY0NDI5MTMy-sign5fbbfb7c2b1a4e42d6c886a2148ae255?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+	},
+]
+
+export default datas
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // <script type="module" 时不能省略.js
-import datas from "./config/blog.js";
+import blogs from "./config/blog.js";
+import musics from "./config/music.js";
+import tabs from "./config/tab.js";
 
 var left = document.getElementById('slice-left')
 var right = document.getElementById('slice-right')
 
 //blog
-for (let e of datas) {
+for (let e of blogs) {
     // 不能这样写<div id="cs"/>
     // css style 能生效
     let div =`
         <div style="margin: 0px;padding: 16px;border-bottom: 1px dotted #ddd;display: flex;align-items: center;">
-            <a class="blog" target="_blank" style="margin: 0px;padding: 0px;color: red;font-size: 16px;color: #4d4d4d;display: flex;align-items: center;" href="` + e.url + `">` + e.title + `</a>
+            <a class="blog" target="_blank" style="margin: 0px;padding: 0px;font-size: 16px;color: #4d4d4d;display: flex;align-items: center;" href="` + e.url + `">` + e.title + `</a>
         </div>
     `
-    if (datas.indexOf(e)<datas.length/2)
+    if (blogs.indexOf(e)<blogs.length/2)
         left.innerHTML += div
     else
         right.innerHTML += div
@@ -28,3 +30,26 @@ for (let i = 0; i < 8; i++) {
     a.setAttribute("href", "image/pic_" + (i + 1) + ".jpg")
     a.setAttribute("target", "_blank")
 }
+
+//music
+var music = document.getElementById('music')
+for (let e of musics) {
+    let div =`
+        <div style="margin: 0px;padding: 16px;border-bottom: 1px dotted #ddd;display: flex;align-items: center;">
+            <a class="blog" target="_blank" style="margin: 0px;padding: 0px;font-size: 16px;color: #4d4d4d;display: flex;align-items: center;" href="` + e.url + `">` + e.title + `</a>
+        </div>
+    `
+    music.innerHTML += div
+}
+
+//tab
+var tab = document.getElementById('tab')
+for (let e of tabs) {
+    let div =`
+        <div style="margin: 0px;padding: 0px 16px;border-right: 1px dotted #ddd;display: flex;align-items: center;">
+            <a class="blog" target="_blank" style="margin: 0px;padding: 0px;font-size: 16px;color: #4d4d4d;display: flex;align-items: center;" href="` + e.url + `">` + e.title + `</a>
+        </div>
+    `
+    tab.innerHTML += div
+}
+
