34cde13086e6ebea893cab76720d06c01d0e443d
笑笑 <819632763@qq.com>
2019-12-27 14:29:01 +0800

    主页

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ for (let e of datas) {
     // css style 能生效
     let div =`
         <div style="margin: 0px;padding: 16px;border-bottom: 1px dotted #ddd;display: flex;align-items: center;">
-            <a class="blog" target="_blank" style="margin: 0px;padding: 0px;color: red;font-size: 14px;color: #4d4d4d;display: flex;align-items: center;" href="` + e.url + `">` + e.title + `</a>
+            <a class="blog" target="_blank" style="margin: 0px;padding: 0px;color: red;font-size: 16px;color: #4d4d4d;display: flex;align-items: center;" href="` + e.url + `">` + e.title + `</a>
         </div>
     `
     if (datas.indexOf(e)<datas.length/2)
