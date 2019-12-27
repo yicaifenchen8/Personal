6a6f66c8ecd2daa823d281f8c0618cb2152b7f1d
笑笑 <819632763@qq.com>
2019-12-27 11:26:34 +0800

    主页

---
+++ config/blog.js
@@ -0,0 +1,92 @@
+var datas = [
+	{
+		title:'Java——GraphicsImage（ 生成图片 绘制图片 绘制文字）',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/103705430'
+	},
+	{
+		title:'SpringBoot学习——HelloWorld（入门项目，用户登录，REST，JPA）',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/103704261'
+	},
+	{
+		title:'android 手势解锁 手势识别 实现原理 思路分析 100行代码实现',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/103693391'
+	},
+	{
+		title:'android 指纹解锁 指纹识别 流程分析 核心代码',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/103692728'
+	},
+	{
+		title:'vue js 终止网络请求的方法',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/103689504'
+	},
+	{
+		title:'java 调用 js',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/103688346'
+	},
+	{
+		title:'java lombok 代码简化 自动生成get set toString方法 万能类型var使用',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/103688201'
+	},
+	{
+		title:'巧思妙解 趣味算法题',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/103687456'
+	},
+	{
+		title:'巧思妙解 一行代码实现颜色按指定顺序排序算法题',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/103686381'
+	},
+	{
+		title:'巧思妙解 一行代码实现生成包含大小写字母和数字的算法题',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/103686131'
+	},
+	{
+		title:'算法 选择排序 冒泡排序',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/82013243'
+	},
+	{
+		title:'算法 线性排序-计数排序',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/46534965'
+	},
+	{
+		title:'算法 线性排序-区间法',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/46534205'
+	},
+	{
+		title:'java udp 客户端',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/46521517'
+	},
+	{
+		title:'java udp 服务端',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/46521495'
+	},
+	{
+		title:'andriod 三星手机拍照各种奇葩问题',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/46521241'
+	},
+	{
+		title:'html 垂直对齐',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/46520741'
+	},
+	{
+		title:'html css 水平 垂直 居中',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/46519139'
+	},
+	{
+		title:'android 圆角 圆形 图片 背景  selector 技巧',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/46518321'
+	},
+	{
+		title:'android ellipsize marquee 滚动文本正确配置',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/46517967'
+	},
+	{
+		title:'android 100行代码实现 仿苹果滑动选择器WheelView',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/46516465'
+	},
+	{
+		title:'android 技术总结 图片优化 sqlite使用 Fragment使用',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/46515793'
+	},
+]
+
+export default datas
---
+++ index.js
@@ -0,0 +1,18 @@
+// <script type="module" 时不能省略.js
+import datas from "./config/blog.js";
+
+var left = document.getElementById('slice-left')
+var right = document.getElementById('slice-right')
+
+for (let e of datas) {
+    // 不能这样写<div id="cs"/>
+    let div =`
+        <div style="margin: 0px;padding: 16px;border-bottom: 1px dotted #ddd;display: flex;align-items: center;">
+            <a class="blog" target="_blank" style="margin: 0px;padding: 0px;color: red;font-size: 14px;color: #4d4d4d;display: flex;align-items: center;" href="` + e.url + `">` + e.title + `</a>
+        </div>
+    `
+    if (datas.indexOf(e)<datas.length/2)
+        left.innerHTML += div
+    else
+        right.innerHTML += div
+}
