835ac06175c53e3a223feb1b8a21c08433fac445
笑笑 <819632763@qq.com>
2019-12-27 11:43:06 +0800

    主页

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var right = document.getElementById('slice-right')
 
 for (let e of datas) {
     // 不能这样写<div id="cs"/>
+    // css style 能生效
     let div =`
         <div style="margin: 0px;padding: 16px;border-bottom: 1px dotted #ddd;display: flex;align-items: center;">
             <a class="blog" target="_blank" style="margin: 0px;padding: 0px;color: red;font-size: 14px;color: #4d4d4d;display: flex;align-items: center;" href="` + e.url + `">` + e.title + `</a>
