489b490608d1a1ec675d377587c4ace9d90ac437
笑笑 <819632763@qq.com>
2020-01-03 21:47:34 +0800

    浏览

--- config/music.js
+++ config/music.js
@@ -1,36 +1,46 @@
 var datas = [
 	{
 		title:'好人一生平安',
-		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTA0OTM2NTM5NQ==-signbfdf1f46f6dd45bbfc4ba25efe14d755?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
-	},
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTA0OTM2NTM5NQ==-signbfdf1f46f6dd45bbfc4ba25efe14d755?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b',
+	    imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1578051940401&di=3a8650cbdc21ed48fa61fed7d25f536f&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%3D580%2Fsign%3D8a3ca4853adbb6fd255be52e3925aba6%2F48bc3518972bd407ee1cc38378899e510eb30909.jpg'
+    },
 	{
 		title:'像鱼',
-		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTAzNjAyNjg0Mw==-sign0fc9d189d9a18aa29c95c62ea7f882f7?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTAzNjAyNjg0Mw==-sign0fc9d189d9a18aa29c95c62ea7f882f7?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b',
+	    imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1578052119811&di=1433e4db4d4e5cfa21d2e68aa9e9613c&imgtype=0&src=http%3A%2F%2Fi2.hdslb.com%2Fbfs%2Farchive%2F337fb555abf23f6e55a8e4e2374dd11c82208757.jpg'
+
 	},
 	{
 		title:'胡广生',
-		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTAzNjAxOTkwNQ==-sign465eb56081a31681c28c1c2336fa55a7?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTAzNjAxOTkwNQ==-sign465eb56081a31681c28c1c2336fa55a7?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b',
+	    imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1578052167622&di=d27f8482231c81ebef3c408d47837e6f&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn10%2F200%2Fw1200h600%2F20181116%2F52d4-hnvukff6797516.jpg'
+
 	},
 	{
 		title:'万爱千恩',
-		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTAwMDM2ODM0MA==-sign465c8309b52ff3820a93213ae17891a2?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataMTAwMDM2ODM0MA==-sign465c8309b52ff3820a93213ae17891a2?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b',
+	    imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1578052274787&di=f4489da71720e8d4378c4d6899cfd983&imgtype=0&src=http%3A%2F%2Fwx2.sinaimg.cn%2Forj360%2F007dH392ly1g28zk6vwaoj30dc0dct9y.jpg'
+
 	},
 	{
 		title:'多年以后',
-		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataOTk3OTI1NDk2-sign643f30db5ffe19d58b03f1d1fc63a390?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataOTk3OTI1NDk2-sign643f30db5ffe19d58b03f1d1fc63a390?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b',
+	    imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1578052330818&di=334b32b3dca8d1fa2bc736a1938433ec&imgtype=0&src=http%3A%2F%2Fy.gtimg.cn%2Fmusic%2Fphoto_new%2FT002R300x300M0000000Jk5b3TFPh3.jpg%3Fmax_age%3D2592000'
+
 	},
 	{
 		title:'成都',
-		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataOTgwNzAyNTgy-signeae40a3c568d2a5ad5ecccff5e9e8aab?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataOTgwNzAyNTgy-signeae40a3c568d2a5ad5ecccff5e9e8aab?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b',
+	    imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1578052351592&di=d810ab28274c85ce753712ff92f3591f&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20181223%2F7a18796d19184d4a8abab3be0cf25d19.gif'
+
 	},
 	{
 		title:'西海情歌',
-		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataOTY0NDI5MTMy-sign5fbbfb7c2b1a4e42d6c886a2148ae255?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b'
+		url:'http://acsing2.kugou.com/sing7/web/share/c0-dataOTY0NDI5MTMy-sign5fbbfb7c2b1a4e42d6c886a2148ae255?u=ODA4ODQxNDM=&us=8723c5a4c1e13a9adc5cf5ced9ccc19b',
+	    imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1578052405546&di=4d99804200f773fb5b26dd069c450995&imgtype=0&src=http%3A%2F%2Fimg.jk51.com%2Fimg_jk51%2F307254015.jpeg'
+
 	},
-	{
-		title:'更多',
-		url:'html/more.html?type=pictrue'
-	},
+
 ]
 
 export default datas
