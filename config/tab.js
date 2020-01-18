a13d98d88be24f81c60b47093d875461e68426f0
gzzc <gzzc123>
2020-01-18 11:10:38 +0800

    年会

--- config/tab.js
+++ config/tab.js
@@ -20,7 +20,7 @@ var datas = [
         type:'music'
     },
     {
-        title:'游记',
+        title:'摄影',
         url:`html/more_grid.html`,
         type:'travel'
     },
--- config/travel.js
+++ config/travel.js
@@ -4,6 +4,11 @@ var datas = [
 		url:'https://blog.csdn.net/yicaifenchen8/article/details/103827263',
 		imgUrl:'https://img-blog.csdnimg.cn/20200103211201368.jpg?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3lpY2FpZmVuY2hlbjg=,size_16,color_FFFFFF,t_70'
 	},
+	{
+		title:'胜利别无选择',
+		url:'https://blog.csdn.net/yicaifenchen8/article/details/104027481',
+		imgUrl:'https://img-blog.csdnimg.cn/20200118101450385.jpg?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3lpY2FpZmVuY2hlbjg=,size_16,color_FFFFFF,t_70'
+	},
 
 ]
 
